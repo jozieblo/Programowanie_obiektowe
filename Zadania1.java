783d2975f085b619f4bdd4ec08e14adb43b1b924
jozieblo <73527284+jozieblo@users.noreply.github.com>
2021-10-14 21:45:14 +0200

    Add files via upload

---
+++ Zadania1.java
@@ -0,0 +1,111 @@
+//zad1
+public class Zadania1
+{
+    public static void main (String[] args)
+    {
+        //zad1
+//        System.out.print("Suma pierwszych 3 miesiecy roku przestepnego wynosi 31+29+31=  ");
+//        System.out.println(31 + 29 + 31);
+
+        //zad2
+//        int suma=0;
+//        for(int i=1;i<=10;i++)
+//       {
+//           suma=i+suma;
+//        }
+//       System.out.println(suma);
+
+        //zad3
+//        int iloczyn=0;
+//        for(int i=1;i<10;i++)
+//        {
+//            System.out.println(i*(i+1));
+//        }
+        //zad4
+//        int pierwszy=1000;
+//        int drugi=pierwszy+(pierwszy*6)/100;
+//        int trzeci=drugi+(drugi*6)/100;
+//        System.out.println(pierwszy);
+//        System.out.println(drugi);
+//        System.out.println(trzeci);
+
+        //zad5
+//        for(int i=0;i<5;i++)
+//        {
+//            System.out.println(|);
+//        }
+        //zad6
+//       System.out.println("  //////");
+//       System.out.println("+'''''''+");
+//       System.out.println("(| o o |)");
+//       System.out.println("  | ^ | ");
+//       System.out.println(" | '-' |");
+//        System.out.println(" +------+");
+
+        //zad7
+//        for (int i=0;i<9;i++)
+//        {
+//            System.out.print("*");
+//
+//        }
+//        System.out.println("");
+//        for (int i=0;i<7;i++)
+//        {
+//            System.out.println("        *");
+//
+//        }
+//        for (int i=0;i<9;i++)
+//        {
+//            System.out.print("*");
+//
+//        }
+        //zad10
+//        System.out.println("Man of Steel");
+//        System.out.println("Winter Soldier");
+//        System.out.println("Matrix");
+
+        //zad11
+//        System.out.println("Płaska jak stół,\n" +
+//                "na którym położona.\n" +
+//                "Nic się pod nią nie rusza\n" +
+//                "i ujścia sobie nie szuka.\n" +
+//                "Nad nią – mój ludzki oddech\n" +
+//                "nie tworzy wirów powietrza\n" +
+//                "i całą jej powierzchnię\n" +
+//                "zostawia w spokoju.\n" +
+//                "\n" +
+//                "Jej niziny, doliny zawsze są zielone,\n" +
+//                "wyżyny, góry żółte i brązowe,\n" +
+//                "a morza, oceany to przyjazny błękit\n" +
+//                "przy rozdzieranych brzegach.\n" +
+//                "\n" +
+//                "Wszystko tu małe, dostępne i bliskie.\n" +
+//                "Mogę końcem paznokcia przyciskać wulkany,\n" +
+//                "bieguny głaskać bez grubych rękawic,\n" +
+//                "mogę jednym spojrzeniem\n" +
+//                "ogarnąć każdą pustynię\n" +
+//                "razem z obecną tuż tuż obok rzeką.\n" +
+//                "\n" +
+//                "Puszcze są oznaczone kilkoma drzewkami,\n" +
+//                "między którymi trudno by zabłądzić.\n" +
+//                "Na wschodzie i zachodzie,\n" +
+//                "nad i pod równikiem –\n" +
+//                "cisza, jak makiem zasiał,\n" +
+//                "a w każdym czarnym ziarnku\n" +
+//                "żyją sobie ludzie.\n" +
+//                "Groby masowe i nagłe ruiny\n" +
+//                "to nie na tym obrazku.\n" +
+//                "\n" +
+//                "Granice krajów są ledwie widoczne,\n" +
+//                "jakby wahały się – czy być czy nie być.\n" +
+//                "\n" +
+//                "Lubię mapy, bo kłamią.\n" +
+//                "Bo nie dają dostępu napastliwej prawdzie.\n" +
+//                "Bo wielkodusznie, z poczciwym humorem\n" +
+//                "rozpościerają mi na stole świat\n" +
+//                "nie z tego świata.");
+
+
+    }
+
+}
